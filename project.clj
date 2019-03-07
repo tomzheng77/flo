738e344d756ada3577e51a49d03b06b0d075d1d0
tom <tom@onmicrosoft.com>
2019-03-08 09:35:58 +1100

    rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monika-clj "0.1.0-SNAPSHOT"
+(defproject sayaka "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
