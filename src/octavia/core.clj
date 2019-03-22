dde7cbb04dd5e88d56997157a080ad94b2167aa7
tom <tom@onmicrosoft.com>
2019-03-22 14:17:48 +1100

    added more methods

--- src/octavia/core.clj
+++ src/octavia/core.clj
@@ -1,7 +1,7 @@
 (ns octavia.core
   (:require [clojure.core.match :refer [match]]
             [clojure.set :as set])
-  (:import (java.time LocalDateTime)))
+  (:import (java.time LocalDateTime ZoneOffset)))
 
 ; a single limiter, a limiter updates the profile of the user
 ; time - the time for this limiter to occur
@@ -14,20 +14,16 @@
  :block-project #{"clojure365"}}
 
 ; object limiters
-; asap - a limiter which should be applied as soon as possible
-; prev - the last limiter which was applied
-; next - the next limiters, the last item will always be treated as an unlock
-{:asap nil
- :prev nil
- :next [{:time          (LocalDateTime/now)
-         :block-login   false
-         :block-host    #{"www.google.com" "anime" "manga"}
-         :block-project #{"clojure365"}}
-        {:time          (LocalDateTime/now)
-         :block-login   false
-         :block-host    #{"www.google.com" "anime" "manga"}
-         :block-project #{"clojure365"}}
-        {:time (LocalDateTime/now)}]}
+; the next limiters, the last item will always be treated as an unlock
+[{:time          (LocalDateTime/now)
+  :block-login   false
+  :block-host    #{"www.google.com" "anime" "manga"}
+  :block-project #{"clojure365"}}
+ {:time          (LocalDateTime/now)
+  :block-login   false
+  :block-host    #{"www.google.com" "anime" "manga"}
+  :block-project #{"clojure365"}}
+ {:time (LocalDateTime/now)}]
 
 (defn add-limits-to-limiter
   "increase the limits of the limiter by the given limits"
@@ -36,6 +32,10 @@
    :block-login   (or (:block-login limiter) (:block-login limits))
    :block-host    (set/union (:block-host limiter) (:block-host limits))
    :block-project (set/union (:block-project limiter) (:block-project limits))})
+
+(defn sort-limiters
+  [limiters]
+  (sort-by #(.toEpochSecond (:time %) (ZoneOffset/UTC)) limiters))
 
 (defn remove-nil [list]
   (filter #(not (nil? %)) list))
@@ -47,33 +47,34 @@
     (not (.isBefore time start))
     (not (.isAfter time end))))
 
-(defn apply-limits-to-list
+(defn apply-limits
   "applies the limits to a list of limiters such that
   the effects of the limit will be respected between {start} and {end}"
-  [limiter-list start end limits]
-  (let [before (filter #(.isBefore (:time %) start) limiter-list)
-        between (filter #(between? (:time %) start end) limiter-list)
-        after (filter #(.isAfter (:time %) end) limiter-list)
-        before-end (last (filter #(.isBefore (:time %) end) limiter-list))]
+  [limiters start end limits]
+  (let [before (filter #(.isBefore (:time %) start) limiters)
+        between (filter #(between? (:time %) start end) limiters)
+        after (filter #(.isAfter (:time %) end) limiters)
+        before-end (last (filter #(.isBefore (:time %) end) limiters))]
     (concat
       before [(assoc limits :time start)]
       (map #(add-limits-to-limiter % limits) between)
       [(assoc before-end :time end)] after)))
 
-(defn add-limits
-  "adds a limiter to the limiters object"
-  [limiters start end limits]
-  (let [limiter-list (remove-nil (concat [(:prev limiters)] (:next limiters)))
-        limiter-list-upd (apply-limits-to-list limiter-list start end limits)]
-    {:next limiter-list-upd}))
+(defn filter-not
+  ([pred] (filter #(not (pred %))))
+  ([pred coll] (filter #(not (pred %)) coll)))
 
-(defn move-to-asap [limiters time-now]
-  (let [removed (filter #(.isBefore (:time %) time-now) (:next limiters))
-        remaining (filter #(not (.isBefore (:time %) time-now)) (:next limiters))
-        asap (last removed)]
-    {:asap asap
-     :prev asap
-     :next remaining}))
+(defn remove-before
+  "removes all limiters before time EXCEPT the last one"
+  [limiters time]
+  (let [removed (filter #(.isBefore (:time %) time) (:next limiters))
+        remaining (filter-not #(.isBefore (:time %) time) (:next limiters))]
+    (concat [(last removed)] remaining)))
+
+(defn limiter-at
+  "finds the limiter which should be effective at {time}"
+  [limiters time]
+  (first (remove-before limiters time)))
 
 (defn mins [x] (.plusMinutes (LocalDateTime/now) x))
 (defn arbitrary [mins])
