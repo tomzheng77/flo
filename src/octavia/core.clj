006dee80c8f4add4071646f7fbccde503a67211a
tom <tom@onmicrosoft.com>
2019-03-23 21:35:46 +1100

    set log level

--- src/octavia/core.clj
+++ src/octavia/core.clj
@@ -1,4 +1,7 @@
 (ns octavia.core
   (:require [clojure.core.match :refer [match]]
-            [clojure.set :as set])
+            [clojure.set :as set]
+            [taoensso.timbre :as timbre])
   (:import (java.time LocalDateTime ZoneOffset)))
+
+(timbre/set-level! :fatal)
