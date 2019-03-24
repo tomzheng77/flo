34c9d212866f7ebc205d127503dec333100fa2b1
tom <tom@onmicrosoft.com>
2019-03-24 23:24:04 +1100

    updated tests

--- test/octavia/limiter_test.clj
+++ test/octavia/limiter_test.clj
@@ -7,9 +7,22 @@
 (defn t [x] (.plusMinutes t0 x))
 (println (add-limiter [] (t 0) (t 10) {}))
 
+(testing "remove-duplicate"
+  (is (= (remove-duplicate [{:time (t 0)} {:time (t 10)} {:time (t 20)}]) [{:time (t 0)} {:time (t 20)}]))
+  (is (= (remove-duplicate
+           (concat (repeat 3 {:time (t 1)})
+                   (repeat 5 {:time (t 2)})
+                   (repeat 7 {:time (t 3)})))
+         [{:time (t 1)}
+          {:time (t 3)}]))
+  (is (= (remove-duplicate
+           (concat (repeat 3 {:time (t 1) :block-host #{"A"}})
+                   (repeat 5 {:time (t 2) :block-host #{"B"}})
+                   (repeat 1 {:time (t 3)})))
+         [{:time (t 1) :block-host #{"A"}}
+          {:time (t 2) :block-host #{"B"}}
+          {:time (t 3)}])))
 
-(is (= (remove-duplicate [{:time (t 0)} {:time (t 10)} {:time (t 20)}])
-       [{:time (t 0)} {:time (t 20)}]))
-
-(is (= (add-limiter [] (t 0) (t 10) nil)
-       [{:time (t 0)} {:time (t 10)}]))
+(testing "add-limiter"
+  (is (= (add-limiter [] (t 0) (t 10) nil)
+         [{:time (t 0)} {:time (t 10)}])))
