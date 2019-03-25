eda27cd12a809016e0092f6b8bdd70a7f4070eb9
tom <tom@onmicrosoft.com>
2019-03-25 18:11:16 +1100

    impl orbit

--- src/limiter/orbit.clj
+++ src/limiter/orbit.clj
@@ -1,7 +1,8 @@
 (ns limiter.orbit
   (:require [limiter.http :refer [start-http-server]]
             [limiter.constants :as c])
-  (:import (java.time LocalDateTime)))
+  (:import (java.time LocalDateTime)
+           (java.util Timer TimerTask)))
 
 ["this is a note"]
 [{:name "A" :time (LocalDateTime/now)}]
@@ -22,19 +23,35 @@
 (defn handle-request [edn]
   (when (= :add-note (:type edn))
     (let [note (:note edn)]
+      (assert (string? note))
       (add-note note)))
   (when (= :add-verification (:type edn))
     (locking verifications
       (let [name (:name edn)
             time (:time edn)]
+        (assert (string? name))
         (assert (not-any? #(= name (:name %)) @verifications))
         (assert (instance? LocalDateTime time))
         (add-verification name time))))
+  (when (= :verify (:type edn))
+    (locking verifications
+      (let [name (:name edn)]
+        (swap! verifications (fn [vs] (filter #(not= name (:name %)) vs))))))
   (when (= :status (:type edn))
     (locking verifications
       (if (not-empty @verifications)
         {:verifications @verifications}
         {:notes @notes}))))
 
+(defn check-verifications []
+  (locking verifications
+    (let [time (LocalDateTime/now)]
+      (let [overdue (filter #(.isBefore time (:time %)) @verifications)]
+        (when (not-empty overdue)
+          (reset! notes [])
+          (reset! verifications []))))))
+
 (defn run []
-  (start-http-server c/orbit-port handle-request))
+  (start-http-server c/orbit-port handle-request)
+  (let [timer (new Timer)]
+    (.schedule timer (proxy [TimerTask] [] (run [] (check-verifications))) 0 1000)))
