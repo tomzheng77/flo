6a9128145322c7fedb364dbfcf68f6e0b32dc521
tom <tom@onmicrosoft.com>
2019-03-28 20:19:22 +1100

    added simple server

--- flo/src/flo/server.clj
+++ flo/src/flo/server.clj
@@ -1,1 +1,18 @@
-(ns flo.server)
+(ns flo.server
+  (:import (com.corundumstudio.socketio Configuration SocketIOServer)
+           (com.corundumstudio.socketio.listener DataListener)))
+
+(defn new-listener [server]
+  (proxy [DataListener] []
+    (onData [client data ack-request]
+      (println data))))
+
+(defn launch []
+  (let [config (new Configuration)]
+    (.setHostname config "localhost")
+    (.setPort config 9092)
+    (let [server (new SocketIOServer config)]
+      (.addEventListener server "hello" String (new-listener server))
+      (.start server)
+      (Thread/sleep Integer/MAX_VALUE)
+      (.stop server))))
