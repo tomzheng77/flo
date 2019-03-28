788eab632926c77f1f29e6588b10ce88ba8cff44
tom <tom@onmicrosoft.com>
2019-03-28 21:43:39 +1100

    use sente

--- flo/src/flo/core.cljs
+++ flo/src/flo/core.cljs
@@ -3,7 +3,7 @@
     [cljs.core.async.macros :as asyncm :refer (go go-loop)])
   (:require
     [cljs.core.async :as async :refer (<! >! put! chan)]
-    [taoensso.sente  :as sente :refer (cb-success?)]))
+    [taoensso.sente :as sente :refer (cb-success?)]))
 
 (enable-console-print!)
 
@@ -13,8 +13,15 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(let [{:keys [chsk ch-recv send-fn state]}
+      (sente/make-channel-socket! "/chsk" {:type :auto})]   ; Note the same path as before; e/o #{:auto :ajax :ws}
+  (def chsk chsk)
+  (def ch-chsk ch-recv)                                     ; ChannelSocket's receive channel
+  (def chsk-send! send-fn)                                  ; ChannelSocket's send API fn
+  (def chsk-state state))                                   ; Watchable, read-only atom
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
