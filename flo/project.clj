b8ef32b0f74fa86742261a8a7bd48086fa26db92
tom <tom@onmicrosoft.com>
2019-04-05 20:19:25 +1100

    upd project

--- flo/project.clj
+++ flo/project.clj
@@ -19,6 +19,7 @@
                  [sablono "0.8.5"]
                  [ring "1.7.1"]
                  [ring/ring-defaults "0.3.2"]
+                 [http-kit "2.3.0"]
                  [compojure "1.6.1"]
                  [cljs-http "0.1.46"]
                  [org.clojure/core.match "0.3.0-alpha5"]
