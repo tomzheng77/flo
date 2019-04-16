f1c100c3400b6634ffaa0d4dde9d6ca5537153d0
Tom Zheng <tomzheng77@gmail.com>
2019-04-16 18:28:14 +1000

    use setText

--- flo/externs.js
+++ flo/externs.js
@@ -4,6 +4,7 @@ Quill.prototype.setSelection = function() {};
 Quill.prototype.enable = function() {};
 Quill.prototype.disable = function() {};
 Quill.prototype.getText = function() {};
+Quill.prototype.setText = function() {};
 Quill.prototype.getContents = function() {};
 Quill.prototype.setContents = function() {};
 Quill.prototype.getBounds = function() {};
