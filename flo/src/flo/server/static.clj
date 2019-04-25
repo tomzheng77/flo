8fbf618cbcd23a39e717c4451bad5e2409e66306
Tom Zheng <tomzheng77@gmail.com>
2019-04-26 09:01:47 +1000

    added login initial html

--- flo/src/flo/server/static.clj
+++ flo/src/flo/server/static.clj
@@ -6,12 +6,16 @@
 (defn base64-encode [to-encode]
   (.encodeToString (Base64/getEncoder) (.getBytes to-encode)))
 
-(def style-css
-  (css [:#app-inner
-        {:margin "0" :display "flex" :flex-direction "column" :justify-content "center"}]
-       [:html :body :#app :#app-inner {:margin 0 :height "100%"}]
-       [:#editor :#editor-read-only
-        {:flex-grow "1" :flex-shrink "1" :display "block" :border-bottom "none" :overflow-y "hidden"}]))
+(defn login-html
+  (html5
+    [:html {:lang "en"}
+     [:head
+      [:meta {:charset "UTF-8"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+      [:link {:rel "icon" :href "cljs-logo-icon-32.png"}]
+      [:style]
+      [:title]]
+     [:body]]))
 
 (defn editor-html [init]
   (html5
@@ -21,7 +25,12 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
       [:link {:rel "icon" :href "cljs-logo-icon-32.png"}]
       [:link {:href "css/highlight/monokai-sublime.css" :rel "stylesheet"}]
-      [:style style-css]
+      [:style
+       (css [:#app-inner
+             {:margin "0" :display "flex" :flex-direction "column" :justify-content "center"}]
+            [:html :body :#app :#app-inner {:margin 0 :height "100%"}]
+            [:#editor :#editor-read-only
+             {:flex-grow "1" :flex-shrink "1" :display "block" :border-bottom "none" :overflow-y "hidden"}])]
       [:title "FloNote"]]
      [:body
       [:pre#init {:style "display: none"} (base64-encode (pr-str init))]
