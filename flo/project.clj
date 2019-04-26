11211d3c63ff062061adb957267246550348a437
Tom Zheng <tomzheng77@gmail.com>
2019-04-26 11:24:51 +1000

    added commons-codec

--- flo/project.clj
+++ flo/project.clj
@@ -38,6 +38,7 @@
                  [org.slf4j/jcl-over-slf4j "1.7.14"]
                  [com.google.guava/guava "27.1-jre"]
                  [org.clojure/data.avl "0.0.18"]
+                 [commons-codec/commons-codec "1.12"]
 
                  ; requires datomic/bin/maven-install
                  [com.datomic/datomic-pro "0.9.5786"]]
