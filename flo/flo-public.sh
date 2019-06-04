e0a5811694a47ea80e5dbadf07275cbb0c13759c
Tom Zheng <tomzheng77@gmail.com>
2019-06-05 01:02:31 +1000

    added port block

--- flo/flo-public.sh
+++ flo/flo-public.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+iptables -A INPUT -p tcp --destination-port 3452 -j DROP
 datomic/bin/transactor dev-transactor-template.properties &
 java -jar flo/flo.jar "ePq6pDeXzjFv" 3452 flo-public
--- flo/flo-server.sh
+++ flo/flo-server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+iptables -A INPUT -p tcp --destination-port 3451 -j DROP
 datomic/bin/transactor dev-transactor-template.properties &
 java -jar flo/flo.jar "ZtrgPz9sdYw9" 3451 flo-server
