d24c4c1e9113d4df469a8bf12a8f69fdc1f19370
Tom Zheng <tomzheng77@gmail.com>
2019-06-05 01:07:57 +1000

    fix

--- flo/flo-server.sh
+++ flo/flo-server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-iptables -A INPUT -p tcp --destination-port 3451 -j DROP
+iptables -A INPUT -p tcp --dport 3451 -s 127.0.0.1 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3451 -j DROP
 datomic/bin/transactor dev-transactor-template.properties &
 java -jar flo/flo.jar "ZtrgPz9sdYw9" 3451 flo-server
