5f9d74335a9438ff932100fe725b26422d217511
Tom Zheng <tomzheng77@gmail.com>
2019-06-05 15:33:37 +1000

    upd script

--- flo/flo-public.sh
+++ flo/flo-public.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-iptables -A INPUT -p tcp --dport 3452 -s 127.0.0.1 -j ACCEPT
-iptables -A INPUT -p tcp --dport 3452 -j DROP
+iptables -A INPUT -p tcp --dport 3451 -s 127.0.0.1 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3451 -j DROP
 datomic/bin/transactor dev-transactor-template.properties &
-java -jar flo/flo.jar "ePq6pDeXzjFv" 3452 flo-public
+java -jar flo/flo.jar "ePq6pDeXzjFv" 3451 flo-public
