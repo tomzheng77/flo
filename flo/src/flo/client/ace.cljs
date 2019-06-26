86e15af674e1aed50330603283a98a1a8613d0bb
Tom Zheng <tomzheng77@gmail.com>
2019-06-27 06:55:01 +1000

    split file

--- flo/src/flo/client/ace.cljs
+++ flo/src/flo/client/ace.cljs
@@ -1,7 +1,8 @@
 (ns flo.client.ace
   (:require [clojure.set :as set]
             [clojure.string :as str]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [flo.client.ace-clickables]))
 
 (defn $ [& args] (apply js/$ args))
 
@@ -104,97 +105,3 @@
   (let [$clickable-layer (.-clickableLayer this)]
     (if $clickable-layer
       (.hide $clickable-layer))))
-
-(def clickable-css
-  "
-  .ace_clickables .ace_clickable_link {
-    color: #589DF6;
-    position: absolute;
-    font-weight: bold;
-    cursor: pointer;
-    z-index: 10;
-    pointer-events: auto;
-    border-radius: 2px;
-    background: rgba(0,0,0,0);
-    opacity: 0;
-    user-select: none;
-    text-decoration: underline;
-  }
-  .ace_clickables .ace_clickable_link:hover {
-    opacity: 1;
-  }
-
-  .ace_content .ace_clickable {
-    pointer-events: auto;
-  }")
-
-(defn get-pos [$el]
-  (let [rect (.getBoundingClientRect (aget $el 0))]
-    {:x (.-left rect) :y (.-top rect)}))
-
-(defn focus-if-not-search [this]
-  (when (nil? (.-searchBox this))
-    (.focus this)))
-
-(defn on-after-render [err renderer]
-  (let [$scroller ($ (.-container renderer))
-        $content ($ (.-content renderer))
-        $clickables (.find $scroller ".ace_clickables")
-        clickable-list (.find $content ".ace_clickable")]
-    (.empty $clickables)
-    (.each
-      clickable-list
-      (fn [_ cl]
-        (let [$cl ($ cl)
-              types (into #{} (map #(subs % 4) (str/split (.-className cl) #"\s+")))
-              text (.text $cl)
-              pos (get-pos $cl)
-              $added ($ (str
-                "<div class='ace_clickable_link' style='left: " (:x pos)
-                "px; top: " (:y pos) "px'>" text "</div>"))]
-          (.appendTo $added $clickables)
-          (.click $added
-            (fn [event]
-              (.stopPropagation event)
-              (.preventDefault event)
-              (rf/dispatch [:click-link types text]))))))))
-
-(defn enable-clickables [this]
-  (js/console.log "Clickables: Enabled")
-  (.on (.-renderer this) "afterRender" on-after-render)
-  (let [$clickable-layer ($ "<div class='ace_layer ace_clickables'></div>")]
-    (set! (.-clickableLayer this) $clickable-layer)
-    (.appendTo $clickable-layer ($ (.-container this)))
-    (hide-clickables this)))
-
-(defn disable-clickables [this]
-  (js/console.log "Clickables: Disabled")
-  (.off (.-renderer this) "afterRender" on-after-render)
-  (-> (.-container this)
-      (js/$)
-      (.find ".ace_layer.ace_clickables")
-      (.remove)))
-
-; add separate clickable layer
-; similar to fs_previews
-; it adds a layer of some input elements
-(js/define
-  "ace/ext/clickables"
-  (clj->js ["ace/editor"])
-  (fn [require exports module]
-    (.importCssString
-      (require "../lib/dom")
-      clickable-css
-      "ace_clickables")
-    (.defineOptions
-      (require "../config")
-      (.. (require "ace/editor") -Editor -prototype)
-      "editor"
-      (clj->js
-        {:enableClickables
-         {:set (fn [val] (this-as this (if val (enable-clickables this) (disable-clickables this))))
-          :value false}}))))
-
-(js/window.require
-  (clj->js ["ace/ext/clickables"])
-  (fn []))
