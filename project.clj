611d593f054b32f62619dc944b2782ae9807397e
tom <tom@365en.onmicrosoft.com>
2020-03-14 10:29:50 +1100

    opt advanced

--- project.clj
+++ project.clj
@@ -82,7 +82,7 @@
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/compiled/flo.js"
                            :main flo.client.client
-                           :optimizations :none
+                           :optimizations :advanced
                            :pretty-print false
                            :npm-deps {:diff "4.0.1"}
                            :install-deps true}}]}
